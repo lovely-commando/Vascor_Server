b4c5029cdc8fa089f49eb64363c387c88c13a80b
SeMin <ohsemin4@gmail.com>
2020-04-05 00:39:02 +0900

    로그인 NodeJS 파일 수정

--- login.js
+++ login.js
@@ -31,16 +31,23 @@ exports.loginProcess = function(req,res){ //로그인 처리
             var hashpassword = crypto.createHash("sha512").update(password+user.u_salt).digest("hex");
             if(hashpassword === user.u_password){
                 console.log("login success");
-
-                mysqlDB.query('select d.color from DEPARTMENT d,USER u where u.u_department = d.u_department and u.u_department = ?',[user.u_department],
-                function(err,results){
-                    console.log(results)
-                    var color = results[0].color
-                    console.log("color : "+color)
-                    login = {"check":"yes","u_email":user.u_email,"u_name":user.u_name,"u_department":user.u_department,"u_id":user.u_id,"color":color}
-                    res.write(JSON.stringify(login));
+                if(user.u_department == null){
+                    login = {"check":"yes","u_email":user.u_email,"u_name":user.u_name,"u_department":user.u_department,"u_id":user.u_id,"color":null}
+                    res.write(JSON.stringify(login))
                     res.end();
-                })
+                }
+                else{
+                    mysqlDB.query('select d.color from DEPARTMENT d,USER u where u.u_department = d.u_department and u.u_department = ?',[user.u_department],
+                    function(err,results){
+                        console.log(results)
+                        var color = results[0].color
+                        console.log("color : "+color)
+                        login = {"check":"yes","u_email":user.u_email,"u_name":user.u_name,"u_department":user.u_department,"u_id":user.u_id,"color":color}
+                        res.write(JSON.stringify(login));
+                        res.end();
+                    })
+                }
+               
             }else{
                 //console.log("비밀번호가 틀림");
                 login = {"check":"wrong"}
